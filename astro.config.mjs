2734b1742e221eb1221f26740e57bd89dd5cbbda
Stefano Cislaghi <s.cislaghi@gmail.com>
2023-10-29 21:07:06 +0000

    feat: various plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 // import markdoc from "@astrojs/markdoc";
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+import pagefind from "astro-pagefind";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()]
+  build: {
+    format: "file",
+  },
+  integrations: [tailwind(), react(), mdx(), compressor(),pagefind()]
 });
